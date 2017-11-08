ad549ec7f5d9b4487adebf3f91d50d8afd5c4897
Pratima Kulkarni <pkulkarni@InComm.com>
2017-11-08 09:29:38 -0500

    poc

---
+++ OrderBackend/src/main/java/com/incomm/medagate/ApplicationConfig.java
@@ -0,0 +1,22 @@
+package com.incomm.medagate;
+
+import javax.inject.Named;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+@Configuration
+public class ApplicationConfig {
+@Named
+static class JerseyConfig extends ResourceConfig {
+public JerseyConfig() {
+this.packages("com.incomm.medagate.rest");
+}
+}
+@Bean
+public RestTemplate restTemplate() {
+RestTemplate restTemplate = new RestTemplate();
+return restTemplate;
+}
+
+}
